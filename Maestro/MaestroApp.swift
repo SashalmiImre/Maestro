1edd3464af8711fd21303254b4bea20272cadf4d
Sashalmi Imre <sashalmi.imre@gmail.com>
2022-12-13 21:37:52 +0100

    Initial Commit

---
+++ Maestro/MaestroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaestroApp.swift
+//  Maestro
+//
+//  Created by Sashalmi Imre on 2022. 12. 13..
+//
+
+import SwiftUI
+
+@main
+struct MaestroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
