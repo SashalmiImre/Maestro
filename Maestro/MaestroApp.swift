52edf8a8c9880cb4f1a08e27e1eba1b70d4c4833
Sashalmi Imre <sashalmi.imre@gmail.com>
2024-12-04 18:28:34 +0100

    Initial Commit

---
+++ Maestro/MaestroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaestroApp.swift
+//  Maestro
+//
+//  Created by Sashalmi Imre on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MaestroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MaestroTests/MaestroTests.swift
@@ -0,0 +1,17 @@
+//
+//  MaestroTests.swift
+//  MaestroTests
+//
+//  Created by Sashalmi Imre on 04/12/2024.
+//
+
+import Testing
+@testable import Maestro
+
+struct MaestroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
